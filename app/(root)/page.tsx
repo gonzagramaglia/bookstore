e11271c74e1cd16e90e86c65baa4e5f403fc8141
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-12 23:00:38 -0300

    frontend: Home page ready

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,15 @@
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
+
 const Home = () => (
   <>
-    <p>Hello World</p>
+    <BookOverview {...sampleBooks[0]} />
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
   </>
 );
 export default Home;
