55e354d1db619bf72432f6fd7a072fa1d5a30da6
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-14 01:21:04 -0300

    backend: neon (serverless postgres db) and drizzle (orm) setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,21 @@
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
-const Home = () => (
-  <>
-    <BookOverview {...sampleBooks[0]} />
-    <BookList
-      title="Latest Books"
-      books={sampleBooks}
-      containerClassName="mt-28"
-    />
-  </>
-);
+const Home = async () => {
+  const res = await db.select().from(users);
+  console.log(JSON.stringify(res, null, 2));
+  return (
+    <>
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+};
 export default Home;
